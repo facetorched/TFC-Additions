ac6c49824bfad0c6811935fbf58c1e19742913b7
nuckable <nuckable@users.noreply.github.com>
2022-06-12 09:16:24 +0200

    Renamed Sandstone to Conglomerate Swapped Dolomite with Slate

--- src/main/java/org/rbh/tfcadditions/Reference/Names.java
+++ src/main/java/org/rbh/tfcadditions/Reference/Names.java
@@ -1,6 +1,5 @@
 package org.rbh.tfcadditions.Reference;
 
-import com.dunk.tfc.api.Enums.EnumFuelMaterial;
 
 /**
  * Created by raymondbh on 15.07.2015.
@@ -8,7 +7,7 @@ import com.dunk.tfc.api.Enums.EnumFuelMaterial;
 public class Names {
 
     public static final String[] STONE_IGIN = new String[]{"Granite", "Diorite", "Gabbro"};
-    public static final String[] STONE_SED = new String[]{"Shale", "Claystone", "Rock Salt", "Limestone", "Conglomerate",
+    public static final String[] STONE_SED = new String[]{"Shale", "Claystone", "Rock Salt", "Limestone", "Sandstone",
             "Dolomite", "Chert", "Chalk"};
     public static final String[] STONE_IGEX = new String[]{"Rhyolite", "Basalt", "Andesite", "Dacite"};
     public static final String[] STONE_MM = new String[]{"Quartzite", "Slate", "Phyllite", "Schist", "Gneiss", "Marble"};
